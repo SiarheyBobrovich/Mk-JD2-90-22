3b39f708af5f465ed3012eefcdbd4762daf8cd6d
Sergey Bobrovich <siarhey1987@gmail.com>
2022-03-23 00:19:09 +0300

    Added javadoc

--- ClassWork/src/main/java/by/it_academy/jd2/ApacheCommonsLang3Checker.java
+++ ClassWork/src/main/java/by/it_academy/jd2/ApacheCommonsLang3Checker.java
@@ -2,6 +2,7 @@ package by.it_academy.jd2;
 
 import org.apache.commons.lang3.StringUtils;
 
+
 public class ApacheCommonsLang3Checker {
     public static boolean isAllLowerCase(final String text) {
         return StringUtils.isAllLowerCase(text);
--- ClassWork/src/main/java/by/it_academy/jd2/Calculator.java
+++ ClassWork/src/main/java/by/it_academy/jd2/Calculator.java
@@ -1,7 +1,16 @@
 package by.it_academy.jd2;
 
+/**
+ * Standard calculator
+ */
 public class Calculator {
 
+    /**
+     * Calculate a + b
+     * @param a - int value
+     * @param b - int value
+     * @return Summary a, b
+     */
     public int add(int a, int b) {
         return a + b;
     }
